7a8e3dbf15b5bab10b312e135b1f84eddfc0906e
ysthakur <45539777+ysthakur@users.noreply.github.com>
2021-08-19 20:07:56 -0400

    Move johnvertisements into index.ts

--- src/index.ts
+++ src/index.ts
@@ -37,6 +37,38 @@ export function execute(): void {
     alert(res);
 }
 
+function popupJohnvertise() {
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+
+    console.log(screenHeight, screenWidth)
+
+    // set size of the popup with a random multiplier
+    const adSize = 1.25 - Math.random();
+    const adWidth = Math.round(732 * adSize);
+    const adHeight = Math.round(94 * adSize);
+
+    // set a random position for the ad
+    let offsetX = "" + Math.floor(Math.random() * (screenWidth - adWidth));
+    const offsetY = Math.floor(Math.random() * (screenHeight - adHeight));
+
+    // if the ad is wider than the screen, get rid of the x offset
+    offsetX = adWidth >= screenWidth ? "" : `right:${offsetX}px;`;
+
+    // spawn the ad
+    document.body.innerHTML += `
+<div style="position:fixed; top:${offsetY}px; ${offsetX} height:${adHeight}px; width:${adWidth}px;" id="popup">
+<img src="./img/x.gif" style="height: 12px; width: 12px; cursor:grab;" onclick="this.parentElement.remove();"></img>
+<iframe src="https://john.mondecitronne.com/embed?ref=http://amiller42.github.io/Cursed-Language" style="margin-left:auto; display:block; max-width:${adWidth}px; width:100%; height:${adHeight}px; border:none;"></iframe>
+</div>`;
+
+    // wait anywhere from 15-90 seconds to spawn another popup
+    setTimeout(popupJohnvertise, Math.floor((Math.random() * 75000) + 15000));
+}
+
+// wait anywhere from 2-10 seconds before the first popup
+setTimeout(popupJohnvertise, Math.floor((Math.random() * 8000) + 2000));
+
 //Set event listeners here because the functions can't be accessed from
 //the HTML directly for some reason.
 //TODO figure out a way to get around that ^
